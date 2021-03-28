39e786162c2029dfbb0b493e30b4e1e44e190973
winsvold <29202094+winsvold@users.noreply.github.com>
2021-03-28 14:03:05 +0200

    tester søkefunksjonalitet

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen, within } from './customTestingLibrary';
 import App from './App';
 import { stationInformationMockData } from "./mocks/stationInformationMockData";
+import userEvent from "@testing-library/user-event";
 
 const mockStations = stationInformationMockData.data.stations;
 
@@ -21,5 +22,33 @@ describe('Bikelist',() => {
       const element = within(bysykkelListe).getByRole("heading", { name: stasjon.name });
       expect(element).toBeInTheDocument();
     })
+  })
+
+  test('viser kun stasjonsnavn som matcher søk', async () => {
+    render(<App />);
+
+    const søkefelt = await screen.findByLabelText(/Søk/i);
+
+    userEvent.type(søkefelt, 'Stasjon 1');
+
+    const bysykkelListe = screen.getByRole('list');
+
+    const treff = within(bysykkelListe).getAllByRole('listitem');
+    expect(treff).toHaveLength(1);
+    within(bysykkelListe).getAllByRole('heading', {name: 'Stasjon 1'});
+  })
+
+  test('viser alle stasjoner om søk tømmes', async () => {
+    render(<App />);
+
+    const søkefelt = await screen.findByLabelText(/Søk/i);
+
+    userEvent.type(søkefelt, 'Stasjon 1');
+    userEvent.clear(søkefelt);
+
+    const bysykkelListe = screen.getByRole('list');
+
+    const treff = within(bysykkelListe).getAllByRole('listitem');
+    expect(treff).toHaveLength(mockStations.length);
   })
 })
